476f91381d1a095f34b349d66ff6e282c330074a
Dejvino <scoutmaster@dejvino.cz>
2024-01-04 06:18:05 +0100

    Improved exception handling. Fixed metronome overflowing ALSA buffers.

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import sys, traceback
 import subprocess
 from threading import Thread
 import time
@@ -8,8 +9,9 @@ from .midi import metronome, event_listener, connect_keyboard_to_synth
 def wrap_runnable(fn):
     try:
         fn()
-    except:
-        print("EXCEPTION")
+    except Exception as e:
+        print("EXCEPTION: " + str(e))
+        traceback.print_exc(file=sys.stdout)
 
 def spawn_synth():
     print("Spawning synthesizer")
--- src/midi.py
+++ src/midi.py
@@ -6,7 +6,12 @@ from alsa_midi import SequencerClient, READ_PORT, WRITE_PORT, NoteOnEvent, NoteO
 from .appconfig import load_common, load_keyboards
 
 def create_client(suffix):
+    # TODO: extract app name
     return SequencerClient("simple-midi-daw_" + suffix)
+
+def find_synth_port(client):
+    # TODO: find the synth based on name
+    return client.list_ports(output=True)[0]
 
 def for_every_keyboard(fn):
     keyboardCfg = load_keyboards()
@@ -24,8 +29,7 @@ def connect_keyboard_to_synth():
     keyboardPortName = keyboardCfg['description']['client_port_name']
     print("Expected MIDI keyboard: " + keyboardClientName)
     client = create_client("keyboard")
-    # TODO: find the synth port
-    synthPort = client.list_ports(output=True)[0]
+    synthPort = find_synth_port(client)
     for_every_keyboard(lambda port : client.subscribe_port(port, synthPort))
 
 def event_listener():
@@ -33,17 +37,17 @@ def event_listener():
     port = client.create_port("midiIn", WRITE_PORT)
     for_every_keyboard(lambda kbd_port : client.subscribe_port(kbd_port, port))
     while True:
-            print("Awaiting event...")
             event = client.event_input()
+            # TODO: handle events
             print(repr(event))
 
 def metronome():
     config = load_common()['metronome']
     client = create_client("metronome")
-    port = client.create_port("midiOut")
+    port = client.create_port("midiOut", READ_PORT)
 
-    # TODO: should match the synth
-    list(map(lambda dest_port: port.connect_to(dest_port), client.list_ports(output=True)))
+    synthPort = find_synth_port(client)
+    port.connect_to(synthPort)
 
     enabled = config.getboolean("enabled")
     bpm = int(config['bpm'])
@@ -68,14 +72,14 @@ def metronome():
                     send_note(client, port, channel, beat_secondary_note, beat_secondary_velocity, wait)
             except:
                 print("EXCEPTION sending metronome note. Exiting.")
-                break
+                raise
         current_beat = (current_beat + 1) % beat_time_measure
 
 def send_note(client, port, channel, note, velocity, wait):
     event1 = NoteOnEvent(note=note, velocity=velocity, channel=channel)
-    client.event_output(event1)
+    client.event_output_buffer(event1)
     client.drain_output()
     time.sleep(wait)
     event2 = NoteOffEvent(note=note, channel=channel)
-    client.event_output(event2)
+    client.event_output_buffer(event2)
     client.drain_output()
