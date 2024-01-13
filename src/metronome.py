c97d1fb86178a8cefb935433a795d709fa34149d
Dejvino <scoutmaster@dejvino.cz>
2024-01-13 21:00:52 +0000

    Re-enable metronome

--- src/metronome.py
+++ src/metronome.py
@@ -1,43 +1,41 @@
 import time
+import mido
 from .appconfig import load_common
 from .appservice import AppService
 from . import midi_mido as midi
+from .midi import MidiEvent
 
 class Metronome(AppService):
-    def __init__(self, inbox):
-        super().__init__(inbox)
+    def __init__(self, inbox, synthInbox):
+        super().__init__(inbox, blocking=False)
+        self.synthInbox = synthInbox
 
     def startup(self):
         self.config = config = load_common()['metronome']
-        #self.client = client = create_client("metronome")
-        #self.port = port = create_output_port(client)
-        #connect_port_to_synth(client, port)
         self.enabled = config.getboolean('enabled')
         self.current_beat = 0
         
     def tick(self):
         config = self.config
-        #client = self.client
-        #port = self.port
         bpm = int(config['bpm'])
-        beat_primary_note = config['beat_primary_note']
-        beat_primary_velocity = config['beat_primary_velocity']
-        beat_secondary_note = config['beat_secondary_note']
-        beat_secondary_velocity = config['beat_secondary_velocity']
+        beat_primary_note = int(config['beat_primary_note'])
+        beat_primary_velocity = int(config['beat_primary_velocity'])
+        beat_secondary_note = int(config['beat_secondary_note'])
+        beat_secondary_velocity = int(config['beat_secondary_velocity'])
         beat_time = config['beat_time'].split("/")
         beat_time_beats = int(beat_time[0])
         beat_time_measure = int(beat_time[1])
-        channel = config['channel']
+        channel = int(config['channel'])
         wait = 60 / bpm
 
         if self.enabled:
-            #print("Tick {}/{}".format(current_beat+1, beat_time_measure))
+            #print("Tick {}/{}".format(self.current_beat+1, beat_time_measure))
             wait = 60 / bpm
             try:
                 if self.current_beat == 0:
-                    midi.send_note(client, channel, beat_primary_note, beat_primary_velocity, wait)
+                    self.send_note(channel, beat_primary_note, beat_primary_velocity, wait)
                 else:
-                    midi.send_note(client, channel, beat_secondary_note, beat_secondary_velocity, wait)
+                    self.send_note(channel, beat_secondary_note, beat_secondary_velocity, wait)
             except:
                 print("EXCEPTION sending metronome note. Exiting.")
                 raise
@@ -50,3 +48,8 @@ class Metronome(AppService):
         print("Message in metronome: " + repr(msg))
         # TODO: other messages?
         self.enabled = not self.enabled
+
+    def send_note(self, channel, note, velocity, wait):
+        self.synthInbox.put(MidiEvent("midi", mido.Message("note_on", note=note, channel=channel, velocity=velocity)))
+        time.sleep(wait)
+        self.synthInbox.put(MidiEvent("midi", mido.Message("note_off", note=note, channel=channel)))
